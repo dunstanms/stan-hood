1ab27720d3aa531fea1d7234150a3b1f9ad68f08
Dunstan M'mbehero <dunmmbehero@gmail.com>
2021-06-06 21:21:25 +0300

    update urls

--- hood/urls.py
+++ hood/urls.py
@@ -1,20 +1,18 @@
-from django.conf.urls import url
-from django.urls import path
+from django.urls import path, include
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
 
-urlpatterns=[
-    url(r'^$',views.home,name = 'home'),
-   url(r'^search/', views.search_results, name='search_results'),
-   url(r'^join/(\d+)',views.join,name = 'join'),
-   url(r'^profile/(\d+)', views.profile, name='profile'),
-   url(r'^editprofile/',views.edit_profile,name = 'edit_profile'),
-   url(r'^exithood/(\d+)',views.exitHood,name = 'exit'),
-   url(r'^newpost/',views.new_post,name = 'new_post'),
-   url(r'^add/buss$', views.new_buss, name='new_buss'),  
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('register/', views.signup, name='signup'),
+    path('account/', include('django.contrib.auth.urls')),
+    path('all-hoods/', views.hoods, name='hood'),
+    path('new-hood/', views.create_hood, name='new-hood'),
+    path('profile/<username>', views.profile, name='profile'),
+    path('profile/<username>/edit/', views.edit_profile, name='edit-profile'),
+    path('join_hood/<id>', views.join_hood, name='join-hood'),
+    path('leave_hood/<id>', views.leave_hood, name='leave-hood'),
+    path('single_hood/<hood_id>', views.single_hood, name='single-hood'),
+    path('<hood_id>/new-post', views.create_post, name='post'),
+    path('<hood_id>/members', views.hood_members, name='members'),
+    path('search/', views.search_business, name='search'),
 ]
-if settings.DEBUG:
-    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-
-
